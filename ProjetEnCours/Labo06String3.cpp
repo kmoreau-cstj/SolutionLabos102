c896f7b4fb1f7bc97c11a25a8abc5cd182cea71b
Karine Moreau <kmoreau@cstj.qc.ca>
2020-11-05 12:32:47 -0500

    Fin du cours 2020-11-05

--- ProjetEnCours/Labo06String3.cpp
+++ ProjetEnCours/Labo06String3.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <string>				// Bibliothèque contenant les fonctions spécifiques aux string : getline
+
+#include "Labo06Fonctions.h"
 using namespace std;			// Pour alléger le code et plus mettre std:: avant les cout, cin, endl, ...
 
 
@@ -20,14 +22,17 @@ int main()
 
 	// demander le nombre à l'utilisateur
 	cout << "Veuillez entrer un nombre entier :";
-	cin >> nombre;
+	//cin >> nombre;
+	nombre = lireEntier();
 
 	// Le redemander si le nombre n'est pas compris entre 1 et 10
 	while (nombre < MIN || nombre >MAX)
 	{
 		cout << "Erreur : le nombre doit être compris entre " << MIN << " et " << MAX << "." << endl;
 		cout << "Veuillez entrer un nombre entier :";
-		cin >> nombre;
+		//cin >> nombre;
+		nombre = lireEntier();
+
 	}
 
 
