d0e9df4c4c9295959da906bc86207ca2ab6c8342
Karine Moreau <kmoreau@cstj.qc.ca>
2020-12-01 13:04:48 -0500

    Fin du cours du 2020-12-01 - Début de l'exercice 2

---
+++ ProjetEnCours/Labo11Exercice2.cpp
@@ -0,0 +1,61 @@
+// But : Lire les informations des contacts à partir d'un fichier txt et générer les étiquettes sur deux
+// colonnes pour les imprimer
+// Auteur : Karine Moreau
+// Date : 2020
+
+
+#include <iostream>
+
+#include "Labo11Exercice2Fonctions.h"
+using namespace std;			// Pour alléger le code et plus mettre std:: avant les cout, cin, endl, ...
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	// Déclaration de constantes
+	
+	
+	// Déclaration des variables au début du programme
+	// 0. Le programme utilise des fichiers, il faut un canal pour chaque fichier
+
+
+
+	// 1. Des variables pour mémoriser les informations fournies par l'utilisateur et/ou par le fichier d'entrée
+	Contact ContactLu;
+
+	// 2. Des variables pour calculer ou afficher les résultats
+
+
+
+	// 3. Des variables intermediaires
+
+
+	// Ouvrir le canal d'entrée
+
+	// Créer le canal de sortie
+
+
+	// On peut avoir à créer un en-tête dans le fichier de sortie
+
+	// On TENTE de lire un premier enregistrement dans le fichier d'entrée
+
+
+	// Tant que le fichier d'entrée n'a pas atteint la fin
+
+		// On applique les traitements (calcul, formater l'affichage, ...) aux données qui ont été lues
+
+		// On TENTE de lire le prochain enregistrement 
+
+
+	// Après avoir traité tous les enregistrements, on peut avoir à écrire le pied de page
+
+	// A la fin on doit fermer tous les fichiers qui ont été ouverts
+
+
+
+
+
+	return 0;
+}
---
+++ ProjetEnCours/Labo11Exercice2Fonctions.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+// Liste des include + using
+#include <iostream>
+
+using namespace std;
+
+// Liste des constantes
+
+
+
+
+// Liste des structures
+// Renault;Paul;125, rue des Lilas;St-Jérôme;J7Z 2G4;QC;12-05-1990;30;450-436-8512;514-705-8745;450-435-6958;PaulRe@hotmail.com;
+struct Contact
+{
+	string nomContact;
+	string prenomContact;
+	string adresseContact;
+	string villeContact;
+	string codePostalContact;
+	string provinceContact;
+	string dateNaissance;
+	int age;
+	string numMaison;
+	string numCell;
+	string numUrgence;
+	string courrielContact;
+
+};
+
+
+
+
+// Liste des prototypes des fonctions
